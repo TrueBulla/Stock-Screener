4054ae482d9872b44d24f2c30f8ca0484a0890ca
TrueBulla <schoof.matt@gmail.com>
2021-07-12 23:09:44 -0400

    Relocated weeklyPlays list, added support for NASDAQ stock tickers

--- Stock_Screener.py
+++ Stock_Screener.py
@@ -1,12 +1,7 @@
 from datetime import datetime, timedelta
-import yfinance as yf
+import yfinance as yf #Requires yfinance-0.1.63
 from pandas_datareader import data as pdr
 import pandas as pd
-
-
-
-
-
 
 #Iterate through stocks to find inside bars
 def screener():
@@ -24,27 +19,34 @@ def screener():
                         Technology
                         Transportation
                         \n""")
-    startDate = datetime.today() - timedelta(days=2)
+
+    startDate = datetime.today() - timedelta(days=1)
     endDate = datetime.today()
+    weeklyPlays = []
+
     yf.pdr_override()
-    totalStocks = pd.read_csv('Stock List.csv')
-    choiceStocks = totalStocks[totalStocks["Sector"] == sector]
+    nyseStocks = pd.read_csv('NYSE Stock List.csv')
+    nasdaqStocks = pd.read_csv('Nasdaq Stock List.csv')
+    bothExchanges = [nyseStocks, nasdaqStocks]
+    concExchanges = pd.concat(bothExchanges)
+    choiceStocks = concExchanges[concExchanges["Sector"] == sector]
     stockList = choiceStocks.Symbol
     while True:
         try:
             for i in stockList:
-               data = pdr.get_data_yahoo(i, start=startDate, end=endDate)
-               currentDate = data.index
-               highPrice = data['High']
-               lowPrice = data['Low']
-               weeklyPlays = []
-               for j in range(len(data)-1):
-                   if highPrice[j] >= highPrice[j+1] and lowPrice[j] <= lowPrice[j+1]:
+                print("Checking price history of {}".format(i))
+                data = pdr.get_data_yahoo(i, start=startDate, end=endDate)
+                highPrice = data['High']
+                lowPrice = data['Low']
+                for j in range(len(data)-1):
+                    if highPrice[j] >= highPrice[j+1] and lowPrice[j] <= lowPrice[j+1]:
                        weeklyPlays.append(i)
-            print("Here is the list of stocks in the {} sector that had inside bars over the last few days. Multiple entries indicate multiple inside bars.\n".format(sector) + str(weeklyPlays))
+                       print(weeklyPlays)
+
+            print("Here is the list of stocks in the {} sector that had inside bars over the last two days.\n".format(sector) + str(weeklyPlays))
             break
         except UnboundLocalError:
-            print("That sector isn't correct. Please try again.")
+            print("That isn't a correct selection. Please try again.")
             screener()
 
 if __name__ == "__main__":
